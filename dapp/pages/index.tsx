57ba07e352fc996fc0d46456e6709acd3842080f
Genie <jeonbg@kakao.com>
2022-05-26 23:13:56 +0900

    Add : Caver-js

--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -1,14 +1,22 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useEffect } from "react";
-import { useAccount } from "../hooks";
+import { useAccount, useCaver } from "../hooks";
 
 const Home: NextPage = () => {
   const { account } = useAccount();
+  const { caver, mintGemTokenContract, saleGemTokenContract } = useCaver();
   useEffect(() => {
-    console.log(account);
-  }, [account]);
-  return <Box>{account}</Box>;
+    console.log(caver);
+  }, [caver]);
+  useEffect(() => {
+    console.log(mintGemTokenContract);
+  }, [mintGemTokenContract]);
+  useEffect(() => {
+    console.log(saleGemTokenContract);
+  }, [saleGemTokenContract]);
+
+  return <Box>{account || ""}</Box>;
 };
 
 export default Home;
